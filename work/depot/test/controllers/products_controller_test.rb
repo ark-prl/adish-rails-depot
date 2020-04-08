d044271c5bef71b8e45803c624f8a703ba5cd46e
Arthur P <arthur.parala@gmail.com>
2020-04-08 16:57:53 +0800

    Uploading Task D: Cart Creation

--- work/depot/test/controllers/products_controller_test.rb
+++ work/depot/test/controllers/products_controller_test.rb
@@ -1,11 +1,3 @@
-#---
-# Excerpted from "Agile Web Development with Rails 5.1",
-# published by The Pragmatic Bookshelf.
-# Copyrights apply to this code. It may not be used to create training material,
-# courses, books, articles, and the like. Contact us if you are in doubt.
-# We make no guarantees that this code is fit for any purpose.
-# Visit http://www.pragmaticprogrammer.com/titles/rails51 for more book information.
-#---
 require 'test_helper'
 
 class ProductsControllerTest < ActionDispatch::IntegrationTest
@@ -15,7 +7,6 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
 
   end
 
-  
   test "should get index" do
     get products_url
     assert_response :success
@@ -26,10 +17,9 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  
   test "should create product" do
     assert_difference('Product.count') do
-      
+
     post products_url, params: { 
         product: { 
           description: @product.description, 
@@ -38,13 +28,12 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
           title: @title,
         }
       }
-    
+
     end
 
     assert_redirected_to product_url(Product.last)
   end
 
-  
   test "should show product" do
     get product_url(@product)
     assert_response :success
@@ -55,9 +44,8 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  
   test "should update product" do
-    
+
     patch product_url(@product), params: { 
         product: { 
           description: @product.description, 
@@ -66,11 +54,18 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
           title: @title,
         }
       }
-    
+
     assert_redirected_to product_url(@product)
   end
 
-  
+  test "can't delete product in cart" do
+    assert_difference('Product.count', 0) do
+      delete product_url(products(:two))
+    end
+
+    assert_redirected_to products_url
+  end
+
   test "should destroy product" do
     assert_difference('Product.count', -1) do
       delete product_url(@product)
